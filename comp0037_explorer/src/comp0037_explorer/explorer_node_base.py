b217df44a97109b3319aa58fa2c9fd13d1aaca19
MichaelLai <wlai2436@gmail.com>
2020-03-05 18:20:10 +0000

    added total time and coverage

--- comp0037_explorer/src/comp0037_explorer/explorer_node_base.py
+++ comp0037_explorer/src/comp0037_explorer/explorer_node_base.py
@@ -235,8 +235,8 @@ class ExplorerNodeBase(object):
             for x in range(width):
                 for y in range(height):
                     cell_status = self.explorer.occupancyGrid.getCell(x, y)
-                    print x, y , cell_status # debug del
-                    if  cell_status != 1 or cell_status != 0: # ie. it is un-determined
+                    # print x, y , cell_status # debug del
+                    if  cell_status != 1. or cell_status != 0.: # ie. it is un-determined
                         uncheckedCells += 1
 
             return 1 - uncheckedCells/totalCells
